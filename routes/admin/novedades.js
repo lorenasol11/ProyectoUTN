347f43f9991497547e1b9b0b65e53135b4efb154
Lorena Bedrune <lorenabedrune@gmail.com>
2022-08-22 00:07:58 -0300

    M6U1

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,13 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
+    var novedades = await novedadesModel.getNovedades();
+
+
+
     res.render('admin/novedades', {
         layout: 'admin/layout',
-        persona: req.session.nombre
+        persona: req.session.nombre,
+        novedades
     });
 });
+
+router.get('/agregar', (req, res, next) => {
+    res.render('admin/agregar', {
+        layout: 'admin/layout'
+    }) //cierra el render
+
+}); // cierra el get
+
+router.post('/agregar', async (req, res, next) => {
+    try {
+        if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != ""){
+            await novedadesModel.insertNovedad(req.body);
+            res.redirect('/admin/novedades')
+    } else {
+        res.render('/admin/agregar', {
+            layout: admin/layout,
+            error: true,
+            message: 'Todos los campos son requeridos'
+        })
+    }
+} catch (error) {
+    console.log(error)
+    res.render('admin/agregar', {
+        layout: admin/layout,
+        error: true,
+        message: 'o se cargo la novedad'
+    })
+}
+})
 
 module.exports = router;
