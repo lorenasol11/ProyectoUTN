cda152ed6a51fcfc367586a86c4162a81b897969
Lorena Bedrune <lorenabedrune@gmail.com>
2022-08-30 21:53:54 -0300

    30/8

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var fileUpload = require('express-fileupload');
 var cors = require('cors');
-var nodemailer = require('nodemailer');
 
 require('dotenv').config();
 var session = require('express-session');
@@ -81,30 +80,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
-router.post('/contacto', async (req, res) => {
-    const mail = {
-      to:'louresol11@gmail.com',
-      subject: 'Contacto Web',
-      html: `${req.body.nombre} se contacto a traves de la web y quiere mas informacion a este correo: ${req.body.email} <br> Además, hizo el siguiente comentario: ${req.body.mensaje} <br> Su tel es: ${req.body.telefono}`
-    }
-
-    const trekking = nodemailer.trekking ({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      }
-    }); //cierra transp
-
-    await trekking.sendMail(mail)
-
-    res.status(201).json({
-      error: false,
-      message: 'Mensaje enviado'
-    });
-    
-}); //cierro post/api
 
 module.exports = app;
